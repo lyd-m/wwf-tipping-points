a9042d9fc32d6149097174cdd53e89ad10f00282
Lydia Marsden <145122215+lyd-m@users.noreply.github.com>
2025-01-28 10:19:37 +0000

    Create test that checks all character categories sum to the same total across both numeric variables - successful.
    Print a summary of each column variable.

--- scripts/comp_data_mangroves_indonesia_shrimp.R
+++ scripts/comp_data_mangroves_indonesia_shrimp.R
@@ -2,8 +2,18 @@ rm(list = ls())
 
 # libraries ------------------
 library(tidyverse)
+library(testthat)
+library(purrr)
 
 # parameters ------------------
+
+# functions ------------------
+
+group_var_by_category <- function(data, category_column, sum_var) {
+  data %>%
+    group_by(!!sym(category_column)) %>%
+    summarise(sum_var = sum(!!sym(sum_var), na.rm = TRUE))
+}
 
 # import ------------------
 
@@ -32,7 +42,52 @@ df_shrimp <- read_csv(list.files(path = "./input-data/company-data/",
 
 # cleaning ------------------
 
+vars_grouping <- c(
+  "country_of_production", "year", "province_of_production", 
+  "kabupaten_of_production", "logistics_hub", "port_of_export", 
+  "exporter", "importer", "country_of_destination", "economic_bloc"
+)
+
+vars_values <- c("volume", "fob")
+
+
+group_var_by_category <- function(data, category_column, sum_var) {
+  data %>%
+    group_by(!!sym(category_column)) %>%
+    summarise(total = sum(!!sym(sum_var), na.rm = TRUE), .groups = "drop")
+}
+
+## check internal consistency of dataset by checking all groups sum to the same
+
+test_that("Group totals match grand total",{
+  results <- map(vars_values, ~ {
+    # loop over each variable (volume, fob)
+    var_value <- .x
+    grand_total <- sum(df_shrimp[[var_value]], na.rm = TRUE)
+    
+    # make sure total value is numeric
+    expect_is(grand_total, "numeric")
+    
+    # within this, loop over each grouping variable (country, exporter, etc)
+    map(vars_grouping, ~ {
+      var_group <- .x
+      grouped_values <- group_var_by_category(df_shrimp, var_group, var_value)
+      
+      # make sure grouped values are numeric
+      expect_is(grouped_values$total, "numeric")
+      
+      grouped_total <- sum(grouped_values$total, na.rm = TRUE)
+      expect_equal(grouped_total, grand_total)
+    })
+  })
+})
+
 # eda ------------------
+
+for (var_ in vars_grouping) {
+  print(var_)
+  print(summary(df_shrimp[[var_]]))
+}
 
 ## how have variables changed through time?
 variable_by_year <- function(data, var){
